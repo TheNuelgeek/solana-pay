90487345402fce374c0b7eb263ec6c40d77ad659
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-04-06 01:49:26 +0100

    Marketplace

---
+++ lib/addresses.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js"
+
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('9Z2YC4EAENgTHx97uNSrrjnZ3yaQuZpWPtR7ZxTghma2') 
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('81WBGakwPHRahe2yyHLt5vJ1HEEbWvXdvQhHSAm6w6zy')
